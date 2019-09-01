8fef8a9efa073c169712326d1de2f70cd507f880
Gabriel Miguel <gmiguel14@gmail.com>
2019-08-31 23:00:13 -0300

    Criando schema da tabela Comment

--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -1,7 +1,11 @@
 import { userQueries } from './resources/user/user.schema';
+import { postQueries } from './resources/post/post.schema';
+import { commentQueries } from './resources/comment/comment.schema';
 
 const Query = `
   type Query {
+    ${commentQueries}
+    ${postQueries}
     ${userQueries}
   }
 `;
